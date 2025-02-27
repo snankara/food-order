e7895d42bf825a69b925515de16e5bc10a05e82b
Sinan Kara <snan_kara_@hotmail.com>
2025-02-27 20:50:45 +0300

    Order entity state changing methods added. Customer and Restaurant aggregate roots added.

---
+++ order-service/domain/core/src/main/java/io/snankara/github/food/order/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package io.snankara.github.food.order.order.service.domain.entity;
+
+import io.snankara.github.food.order.domain.entity.AggregateRoot;
+import io.snankara.github.food.order.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
