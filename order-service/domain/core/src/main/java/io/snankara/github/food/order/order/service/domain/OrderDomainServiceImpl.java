b95e271bdeea2d939ee4607e786f3a615ffff6ac
Sinan Kara <snan_kara_@hotmail.com>
2025-03-04 22:25:24 +0300

    OrderDomainService implemented.

---
+++ order-service/domain/core/src/main/java/io/snankara/github/food/order/order/service/domain/OrderDomainServiceImpl.java
@@ -0,0 +1,74 @@
+package io.snankara.github.food.order.order.service.domain;
+
+import io.snankara.github.food.order.domain.exception.DomainException;
+import io.snankara.github.food.order.order.service.domain.entity.Order;
+import io.snankara.github.food.order.order.service.domain.entity.Product;
+import io.snankara.github.food.order.order.service.domain.entity.Restaurant;
+import io.snankara.github.food.order.order.service.domain.event.OrderCancelledEvent;
+import io.snankara.github.food.order.order.service.domain.event.OrderCreatedEvent;
+import io.snankara.github.food.order.order.service.domain.event.OrderPaidEvent;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Slf4j
+public class OrderDomainServiceImpl implements OrderDomainService {
+
+    private static final String UTC = "UTC";
+
+    @Override
+    public OrderCreatedEvent validateAndInitiate(Order order, Restaurant restaurant) {
+        validateRestaurant(restaurant);
+        setOrderProductInformation(order, restaurant);
+        order.validateOrder();
+        order.initializeOrder();
+        log.info("Order with id: {} is initiated", order.getId().getValue());
+
+        return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public OrderPaidEvent pay(Order order) {
+        order.pay();
+        log.info("Order with id: {} is paid", order.getId().getValue());
+
+        return new OrderPaidEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public void approve(Order order) {
+        order.approve();
+        log.info("Order with id: {} is approved", order.getId().getValue());
+    }
+
+    @Override
+    public OrderCancelledEvent cancelPayment(Order order, List<String> failureMessages) {
+        order.initCancel(failureMessages);
+        log.info("Order payment is cancelling for order id: {}", order.getId().getValue());
+
+        return new OrderCancelledEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public void cancelOrder(Order order, List<String> failureMessages) {
+        order.cancel(failureMessages);
+        log.info("Order with id: {} is cancelled", order.getId().getValue());
+    }
+
+    private void validateRestaurant(Restaurant restaurant) {
+        if (!restaurant.isActive())
+            throw new DomainException("Restaurant with id " + restaurant.getId().getValue() +
+                    " is currently not active.");
+    }
+
+    //Big O complexity
+    private void setOrderProductInformation(Order order, Restaurant restaurant) {
+        order.getItems().forEach(orderItem -> restaurant.getProducts().forEach(restaurantProduct -> {
+            Product currentProduct = orderItem.getProduct();
+            if (currentProduct.equals(restaurantProduct))
+                currentProduct.updateWithConfirmedNamedAndProduct(restaurantProduct.getName(), restaurantProduct.getPrice());
+        }));
+    }
+}
